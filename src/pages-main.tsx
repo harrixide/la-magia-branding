abb5d94281fbcd44eaa2a997022cbe7192e1f314
harrixide <acs.aslam@gmail.com>
2026-08-06 21:14:09 -0400

    Add client entry for GitHub Pages

---
+++ src/pages-main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import { Route as IndexRoute } from "./routes/index";
+
+const IndexComponent = IndexRoute.options.component as React.ComponentType;
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <IndexComponent />
+  </React.StrictMode>,
+);
